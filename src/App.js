b7dd53b236fbb195b3100af70db2a4b7090db8a4
solahidris <emailsolah@gmail.com>
2023-05-18 17:37:43 +0800

    added display using usestate

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ function App() {
   const [operatorValue, setOperatorValue] = useState(null);
   const [secondValue,setSecondValue] = useState(null);
   const [totalValue,setTotalValue] = useState(null);
+  const [displayValue, setDisplayValue] = useState(null);
 
   const buttonHandler = (e) => {
     const value = e.target.value;
     if (!isNaN(value) && operatorValue === null) {
       setFirstValue(value);
+      setDisplayValue(value);
     } else if (firstValue !== null && isNaN(value) && value !== "=" && secondValue === null) {
       setOperatorValue(value);
     } else if (!isNaN(value) && firstValue !== null && operatorValue !== null) {
       setSecondValue(value);
-    } else if (value === "=") {
+      setDisplayValue(value);
+    } else if ((value === "=") && (secondValue !== null)) {
       let result;
       if (operatorValue === "+") {
         result = parseInt(firstValue) + parseInt(secondValue);
@@ -31,7 +34,8 @@ function App() {
         result = parseInt(firstValue) / parseInt(secondValue);
       }
       setTotalValue(result);
-      setFirstValue(null);
+      setFirstValue(result); // Allow continue calculation
+      setDisplayValue(result);
       setOperatorValue(null);
       setSecondValue(null);
     }
@@ -42,26 +46,33 @@ function App() {
 
       <Header />
 
-      <div>
-        <label className="flex justify-center p-2 bg-white text-black">{secondValue}</label>
-        <label className="flex justify-center p-2 bg-black text-white">{firstValue}</label>
+      <div className="my-5 mx-5">
+        <div>
+            <label className="flex justify-center p-2 mb-5 bg-black text-white">{displayValue}</label>
+        </div>
+        
+        <label className="flex justify-center p-2 bg-blue-500 text-white">{firstValue}</label>
+        <label className="flex justify-center p-2 bg-green-500 text-black">{secondValue}</label>
+        <label className="flex justify-center p-2 bg-yellow-500 text-black">{totalValue}</label>
+      </div>
+      
+      <div className="mx-5">
+        <button value="1" onClick={buttonHandler} className="p-5 bg-white mr-2">1</button>
+        <button value="2" onClick={buttonHandler} className="p-5 bg-white mr-2">2</button>
+        <button value="3" onClick={buttonHandler} className="p-5 bg-white mr-2">3</button>
+
+        <button value="+" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">+</button>
+        <button value="-" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">-</button>
+        <button value="*" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">*</button>
+        <button value="/" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">/</button>
+        <button value="=" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">=</button>
       </div>
 
-      <button value="1" onClick={buttonHandler} className="p-5 bg-white mr-2">1</button>
-      <button value="2" onClick={buttonHandler} className="p-5 bg-white mr-2">2</button>
-      <button value="3" onClick={buttonHandler} className="p-5 bg-white mr-2">3</button>
-
-      <button value="+" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">+</button>
-      <button value="-" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">-</button>
-      <button value="*" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">*</button>
-      <button value="/" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">/</button>
-      <button value="=" onClick={buttonHandler} className="p-5 bg-yellow-300 mr-2">=</button>
-      
       <div className="flex justify-center mt-5">
-      <label className="p-5 bg-blue-500 text-blue">1V) {firstValue}</label>
-      <label className="p-5 bg-green-500 text-black">OV) {operatorValue}</label>
-      <label className="p-5 bg-yellow-500 text-black">2V) {secondValue}</label>
-      <label className="p-5 bg-yellow-500 text-black">TV) {totalValue}</label>
+        <label className="p-5 bg-blue-500 text-blue">1V) {firstValue}</label>
+        <label className="p-5 bg-red-500 text-black">OV) {operatorValue}</label>
+        <label className="p-5 bg-green-500 text-black">2V) {secondValue}</label>
+        <label className="p-5 bg-yellow-500 text-black">TV) {totalValue}</label>
       </div>
 
     </div>
